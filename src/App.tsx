b36f2ccd54f8db5c61563c133b18b0690a3731d1
Artur-Petrov <petrov.artur02@gmail.com>
2026-02-03 22:33:00 +0200

    homework1/task1/gi01MZ6v styles added

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App=()=> {
     <>
         <ul>
           {
-            coursesTitleArray.map((value, index) => <li key={index}>{value}</li>)
+            coursesTitleArray.map((value, index) => <li className="bg-sky-500/75 m-10 p-10 border-4 border-double border-purple-500 hover:bg-purple-500 hover:border-sky-500 transition-all duration-600 ease-in-out" key={index}>{value}</li>)
           }
         </ul>
     </>
