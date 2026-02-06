e60296765c054e5bf4a1b15037fe180c65f7565f
Artur-Petrov <petrov.artur02@gmail.com>
2026-02-07 00:36:20 +0200

    lesson2/useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import {useState} from "react";
 
-function App() {
+const App = () => {
+  let [counter, setCounter] = useState(0);
+  //useState це хук приймає початкове значення, повертає масив з 2 об'єктами
+  // 1 об'єкт це getter, 2 - setter\
+  // дозволяє нам зберігати фіксувати і змінювати стан нашої компоненти
+    return (
+        <div>
+          <h2>{counter}</h2>
+            <button onClick={()=>{
+              setCounter(++counter);
+            }}>increment</button>
+            <button onClick={()=>{
+              setCounter(--counter);
+            }}>decrement</button>
+        </div>
+    );
+};
 
-  return (
-    <></>
-  )
-}
-
-export default App
+export default App;
