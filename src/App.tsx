d0e7677f6f20ea10a5616c3c26a5d46abbfac3de
Artur-Petrov <petrov.artur02@gmail.com>
2026-02-07 01:28:45 +0200

    lesson2/useEffect + fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import './App.css'
+import {useEffect, useState} from "react";
+import type {IUser} from "./models/IUser.ts";
+import UserComponent from "./UserComponent.tsx";
 
-function App() {
+const App = () => {
 
-  return (
-    <></>
-  )
-}
+    const [users, setUsers] = useState<IUser[]>([]);
 
-export default App
+    // буде нескінченний цикл так як useState буде rerender сторінку тому цей варіант не підходить
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then((response) => response.json())
+    //     .then((response) => {
+    //         setUsers(response);
+    //     });
+
+
+    // useEffect має два аргументи 1 - callback функцію і 2 - масив залежностей
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((response) => {
+                setUsers(response);
+            });
+        // useEffect відповідно має return (unsubscribe function) яка є callback
+        return()=>{
+            console.log('done');
+        }
+    }, [])
+    // якщо масив пустий [] то useEffect виконається тільки 1 раз
+    return (
+        <div>
+            {
+                users.map((user) => (<UserComponent key={user.id} item={user}/>))
+            }
+        </div>
+    );
+};
+
+export default App;
