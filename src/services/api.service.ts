cf844b7ecd5180e49c782f9b43ac0ed397c113a2
Artur-Petrov <petrov.artur02@gmail.com>
2026-02-09 20:24:57 +0200

    lesson2/env files

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,12 +1,12 @@
 import type {IUser} from "../models/IUser.ts";
 
 const getUsers = async ():Promise<IUser[]> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users')
+    return await fetch(import.meta.env.VITE_API_URL)
         .then((response) => response.json());
-}
 
+}
 const getUser = async (id: string): Promise<IUser> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users' + id)
+    return await fetch(import.meta.env.VITE_API_URL + '/' + id)
         .then((response) => response.json());
 }
 
