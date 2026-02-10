beb6c8bd1ca36aa16ca97db5569b821d3772b510
Artur-Petrov <petrov.artur02@gmail.com>
2026-02-10 18:52:02 +0200

    homework2/task1/ohm4AivCEBv styles added

--- src/components/todo-component/TodoComponent.tsx
+++ src/components/todo-component/TodoComponent.tsx
@@ -7,12 +7,12 @@ type TodoModel = {
 
 export const TodoComponent: FC<TodoModel> = ({todo}) => {
     return (
-        <>
+        <div className="flex gap-5 flex-col m-5 p-10 bg-purple-500 text-xl text-white border-solid rounded-xl">
         <div>{todo.title}</div>
         <div>{todo.id}</div>
         <div>{todo.userId}</div>
         <div>{todo.completed.toString()}</div>
-        </>
+        </div>
     );
 };
 
