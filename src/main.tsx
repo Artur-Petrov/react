599cedf86b987128fdf8c524f358961e9d0c758c
Artur-Petrov <petrov.artur02@gmail.com>
2026-02-20 00:10:56 +0200

    homework3/task1_2/chszHaH9

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {router} from "./router/router.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.tsx";
+import UsersPage from "./pages/UsersPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import PostsPage from "./pages/PostsPage.tsx";
+import CommentsPage from "./pages/CommentsPage.tsx";
+import ProductsPage from "./pages/ProductsPage.tsx";
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={router}/>)
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route path={''} element={<HomePage/>}/>
+                <Route path={'users'} element={<UsersPage/>}/>
+                <Route path={'posts'} element={<PostsPage/>}/>
+                <Route path={'comments'} element={<CommentsPage/>}/>
+                <Route path={'products'} element={<ProductsPage/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>)
