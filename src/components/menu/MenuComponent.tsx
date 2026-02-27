8f9836f3fcac1fdbff7b0501d30279c9b880997f
Artur-Petrov <petrov.artur02@gmail.com>
2026-02-27 23:11:35 +0200

    homework3/task6/knZgVIwvqd done

---
+++ src/components/menu/MenuComponent.tsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom";
+
+const MenuComponent = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to={'users'}>Users</Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default MenuComponent;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router-dom";
+import MenuComponent from "../components/menu/MenuComponent.tsx";
 
 const MainLayout = () => {
     return (
         <div>
             <div>Main Layout</div>
+            <hr/>
+            <MenuComponent/>
+            <hr/>
             <Outlet/>
         </div>
     );
