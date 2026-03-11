018d88043d7ece77a37c50ad89bfb568c8793a85
Artur-Petrov <petrov.artur02@gmail.com>
2026-03-12 00:56:35 +0200

    lesson5/uncontrolled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import FormComponent from "./component/FormComponent.tsx";
 
 function App() {
 
-  return (
-    <></>
-  )
+    return (
+        <>
+            {/*<div contentEditable={true}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos in quos*/}
+            {/*    rem sequi ut? Accusantium cupiditate ipsa ipsam iste minus odio provident quidem reprehenderit soluta*/}
+            {/*    tempora. Et, facere hic! Soluta.*/}
+            {/*</div>*/}
+            <FormComponent/>
+        </>
+    )
 }
 
 export default App
