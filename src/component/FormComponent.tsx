3e7d1fdf2be9de343eeabd76ba5f1eb51cbd45ce
Artur-Petrov <petrov.artur02@gmail.com>
2026-03-16 14:32:55 +0200

    lesson5/useForm

--- src/component/FormComponent.tsx
+++ src/component/FormComponent.tsx
@@ -1,46 +1,24 @@
-import {type FormEvent, useState} from "react";
+import {useForm} from "react-hook-form";
 
-
-interface IFormProps {
-    username: string;
-    password: string;
+interface IFormProps{
+    username: string,
+    password: string,
+    age: number
 }
 
 const FormComponent = () => {
 
-    const [formState, setFormState] = useState<IFormProps>({
-        username:'',
-        password:''
-    })
+    const {handleSubmit, register}  = useForm<IFormProps>();
 
-    const handlerSubmit = (e:FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        let user = {
-            username : formState.username,
-            password: formState.password
-        }
-        console.log(user)
-    };
-
-    // const handleUsernameChange = (e:FormEvent<HTMLInputElement>) => {
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, username:input.value});
-    // };
-    // const handlePasswordChange = (e:FormEvent<HTMLInputElement>) => {
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, password:input.value});
-    // };
-
-    const handleInputChange = (e:FormEvent<HTMLInputElement>) => {
-        const input = e.target as HTMLInputElement;
-        setFormState({...formState, [input.name]:input.value});
+    const customHandler = (formDataProps:IFormProps)=>{
+        console.log(formDataProps)
     };
     return (
         <div>
-            <form onSubmit={handlerSubmit}>
-                <input type="text" name={'username'} value={formState.username} onChange={handleInputChange}/>
-                <input type="text" name={'password'} value={formState.password} onChange={handleInputChange}/>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <input type="text" {...register('username')}/>
+                <input type="text" {...register('password')}/>
+                <input type="number" {...register('age')}/>
                 <button>send</button>
             </form>
         </div>
