7f9507a4017906e299ce84fdc02707f52ba10fcd
Artur-Petrov <petrov.artur02@gmail.com>
2026-04-13 16:03:16 +0300

    lesson7/MyContext

---
+++ src/components/LeftBranch.tsx
@@ -0,0 +1,14 @@
+import LeftBranchA from "./LeftBranchA.tsx";
+
+const LeftBranch = () => {
+    return (
+        <div>
+            Left Branch
+            <p>
+                <LeftBranchA/>
+            </p>
+        </div>
+    );
+};
+
+export default LeftBranch;
